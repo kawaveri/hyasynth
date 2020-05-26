2cde5d2ad32add8f6e7330b546e43c43e4188985
kawabe-esw <kawabe@e-sw.co.jp>
2020-05-26 23:01:57 +0900

    Getwildモデルを実装。

---
+++ getwild/app/Getwild.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Getwild extends Model
+{
+
+    /**
+     * getした組み合わせがwild&toughか判定します。
+     *
+     * @param string $word1 文字列１
+     * @param string $word2 文字列２
+     * @return bool 判定結果
+     */
+    public function get(string $word1, string $word2)
+    {
+        return $word1 === 'wild' && $word2 === 'tough';
+    }
+}

753bccf72dfc07bef289158996fabae6e6e5235f
kawabe-esw <kawabe@e-sw.co.jp>
2020-06-10 09:24:23 +0900

    テストassertionの書き方を変更。

--- getwild/tests/Feature/homepageControllerTest.php
+++ getwild/tests/Feature/homepageControllerTest.php
@@ -10,13 +10,15 @@ class homepageControllerTest extends TestCase
 {
     /**
      * 昔の個人ホームページにありがちだったことをテストします。
+     * @link https://getnews.jp/archives/59627
      *
      * @return void
      */
     public function testHomepage()
     {
-        $response = $this->get('/homepages');
-
-        $response->assertStatus(200);
+        //
+        $response = $this->get('/homepages')
+            ->assertStatus(200)
+            ->assertSee('ホームページ');
     }
 }
